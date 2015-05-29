8f931be8fbac1eb72b150fb347fa40e074842e47
Yanxing Wang <demiwangya@gmail.com>
2015-05-29 01:10:24 -0700

    first commit

---
+++ app/src/main/java/com/fgdsb/visabulletin/app/AboutFragment.java
@@ -0,0 +1,20 @@
+package com.fgdsb.visabulletin.app;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yanxingwang on 5/29/15.
+ */
+
+public class AboutFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+}
