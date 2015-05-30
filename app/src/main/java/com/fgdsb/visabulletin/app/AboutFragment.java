88f80cfa5bb5d9dce8e07f56e2ddd11ade6df5a0
Yanxing Wang <demiwangya@gmail.com>
2015-05-29 17:43:16 -0700

    'about' fragment

--- app/src/main/java/com/fgdsb/visabulletin/app/AboutFragment.java
+++ app/src/main/java/com/fgdsb/visabulletin/app/AboutFragment.java
@@ -2,19 +2,30 @@ package com.fgdsb.visabulletin.app;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-/**
- * Created by yanxingwang on 5/29/15.
- */
+import android.widget.TextView;
 
 public class AboutFragment extends Fragment {
+
+    TextView mLink;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View ret = inflater.inflate(R.layout.fragment_about, container, false);
+
+        mLink = (TextView) ret.findViewById(R.id.git_repo);
+        if (mLink != null) {
+            mLink.setMovementMethod(LinkMovementMethod.getInstance());
+            mLink.setText(Html.fromHtml(getResources().getString(R.string.git_repo)));
+        }
+
+        return ret;
     }
 }
